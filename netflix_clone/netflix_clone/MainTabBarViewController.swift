b74c0b2251da00e5b83bb9e78ae4a133a7a95f13
truong.thien <95615938+nguyenhuutu270402@users.noreply.github.com>
2023-04-25 21:06:26 +0700

    update 1

---
+++ netflix_clone/netflix_clone/MainTabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  netflix_clone
+//
+//  Created by Thien on 25/04/2023.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+        
+    }
+
+
+}
+
